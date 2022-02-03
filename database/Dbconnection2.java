7ae472345bdf8ea26ca0bd8e3d32726266eaa31b
james <jamesmahato10@gmail.com>
2022-02-03 22:55:26 +0545

    Admin view JTable added with button functionality in Home Dashboard

---
+++ database/Dbconnection2.java
@@ -0,0 +1,73 @@
+package database;
+import java.sql.*;
+
+public class Dbconnection2 {
+
+    public Connection con;
+    Statement st;
+    ResultSet rows;
+    int val;
+
+    public Dbconnection2() {
+        // register the driver class
+        try {
+            String username = "root";
+            String password = "root";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // create the connection object
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Ghumantey?characterEncoding=utf8&useSSL=false&autoReconnect=true",
+                    username, password);
+            if (con != null) {
+                System.out.println("Connected to Ghumantey Database");
+            } else {
+                System.out.println("Error connecting Database");
+            }
+
+            // creating statement object
+            st = con.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    // method to insert data using prepared statement
+  public int maniulate(PreparedStatement st2) {
+
+    try {
+        val = st.executeUpdate(null);
+        // con.close();
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+
+    return val;
+}
+    // Used for insert, update, delete
+    public int maniulate(String query) {
+        try {
+            val = st.executeUpdate(query);
+            con.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return val;
+    }
+
+
+// Used for select
+public ResultSet retrieve(String query) {
+    try {
+        rows = st.executeQuery(query);
+        // con.close();
+    } catch (SQLException throwables) {
+        throwables.printStackTrace();
+    }
+    return rows;
+}
+// public static void main(String[] args) {
+//     new Dbconnection();
+// }
+
+
+}
---
+++ view/view.java
@@ -0,0 +1,71 @@
+package view;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import controller.StaysController2;
+import model.Stays2;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.List;
+
+public class view extends JFrame   {
+
+    Object[] columns = { "StaysID","Name","ArrivalDate", "DepartureDate", "TotalnumberofAdults", "DepartureDate" };
+    String data[][];
+    JTable table;
+    DefaultTableModel model;
+
+    public view() {
+
+        fillArray();
+
+        setTitle("view");
+        setSize(600, 600);
+
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        setLayout(new BorderLayout());
+
+        // JLabel lblUsername = new JLabel("Welcome " + customer.getCustFname() +
+        //         " " + customer.getCustLname());
+
+        // lblUsername.setFont(new Font("Arial", Font.BOLD, 20));
+        // lblUsername.setHorizontalAlignment(JLabel.CENTER);
+
+        // Bind model to JTable
+        table = new JTable(model);
+
+        table.setFont(new Font("Serif", Font.ITALIC, 16));
+        table.setSelectionBackground(Color.green);
+        table.getTableHeader().setBackground(Color.orange);
+        table.getTableHeader().setFont(new Font("Serif", Font.BOLD, 16));
+        add(new JScrollPane(table), BorderLayout.CENTER);
+
+        // add(lblUsername, BorderLayout.NORTH);
+        setVisible(true);
+    }
+    
+    private void fillArray() {
+        StaysController2 controller = new StaysController2();
+        List<Stays2> lstStays = controller.getAllStays();
+        
+        data = new String[lstStays.size()][6];
+
+        for (int i = 0; i < lstStays.size(); i++) {
+            data[i][0] = Integer.toString(lstStays.get(i).getStaysID());
+            data[i][1] = lstStays.get(i).getName();
+            data[i][2] = lstStays.get(i).getArrivalDate();
+            data[i][3] = lstStays.get(i).getDepartureDate();
+            data[i][4] = Integer.toString(lstStays.get(i).getTotalnumberofAdults());
+            data[i][5] = Integer.toString(lstStays.get(i).getTotalnumberofChildren());
+        }
+
+        model = new DefaultTableModel(data, columns);
+
+    }
+
+
+
+}
