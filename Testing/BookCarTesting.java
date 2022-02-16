1d2e65b84a6eb1734302e0a091e19f9bb175fde6
JamesMahato <84493570+JamesMahato@users.noreply.github.com>
2022-02-16 12:04:00 +0545

    Book CarTestin  added

---
+++ Testing/BookCarTesting.java
@@ -0,0 +1,35 @@
+package test;
+
+import controller.Carcontroller;
+import controller.TouristController;
+import database.Dbconnection;
+import model.Car;
+import model.Tourist;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BookCarTesting {
+    Car car;
+    Carcontroller  carcontroller ;
+    @Before
+    public void init(){
+        car = new Car();
+        carcontroller = new Carcontroller();
+    }
+    @Test
+    public void testRegisterTourist(){
+        car = new Car("55","chitwan","pokhara","02/02/2002","02/07/2002");
+        int actualResult = carcontroller.registercar(car);
+        int expectedResult =1;
+        Assert.assertEquals(actualResult,expectedResult);
+    }
+    @After
+    public void tearDown(){
+        car = null;
+        carcontroller = null;
+    }
+
+
+}
