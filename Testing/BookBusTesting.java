00d8789fdb4a7620237b498838966ba1f38d2c72
JamesMahato <84493570+JamesMahato@users.noreply.github.com>
2022-02-16 12:03:00 +0545

    BookBusTesting added

---
+++ Testing/BookBusTesting.java
@@ -0,0 +1,35 @@
+package test;
+
+import controller.Buscontroller;
+import controller.TouristController;
+import database.Dbconnection;
+import model.Bus;
+import model.Tourist;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BookBusTesting {
+    Bus  bus ;
+    Buscontroller buscontroller;
+    @Before
+    public void init(){
+        bus = new Bus();
+        buscontroller = new Buscontroller();
+    }
+    @Test
+    public void testRegisterTourist(){
+        bus = new Bus("66","pokhara","Ktm","01/01/2002","01/05/2002");
+        int actualResult = buscontroller.registerbus(bus);
+        int expectedResult =1;
+        Assert.assertEquals(actualResult,expectedResult);
+    }
+    @After
+    public void tearDown(){
+        bus = null;
+        buscontroller = null;
+    }
+
+
+}
